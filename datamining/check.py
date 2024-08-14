8baed61d07dd8c381b17f7877f800de72b7b046d
filiphalys02 <filiphalys02@gmail.com>
2024-08-14 19:52:32 +0200

    Function check_numeric data (argument use, description, outlier counter, iqr)

--- datamining/check.py
+++ datamining/check.py
@@ -3,7 +3,34 @@ from datamining._errors import _validate_argument_types
 
 
 @_validate_argument_types
-def check_numeric_data(df: pd.DataFrame, round: int = 1):
+def check_numeric_data(df: pd.DataFrame, round: int = 1, use: bool = False):
+    """
+    The function summarizes numerical columns in a data frame using statistical measures.
+    :param df: pandas DataFrame -> Input Data Frame
+    :param round: int -> The number of decimal places to which the function will round the measures.
+    :param use: boolean -> To use the output as a pandas Data Frame, set to True.
+    :return: str -> If param use is False
+             pandas DataFrame -> If param use is True
+
+            Output measures:
+            NAME -> Column name
+            TYPE -> Data type
+            NaN -> Number of missing values
+            AVG -> Mean
+            Q25 -> 25th percentile
+            Q50 -> 50th percentile / median
+            Q75 -> 75th percentile
+            IQR -> Interquartile range
+            MIN -> Minimum value
+            MAX -> Maximum value
+            STD -> Standard deviation
+            SUM -> Sum of values
+            LOW OUT -> Number of lower outliers
+            UPP OUT -> Number of upper outliers
+    """
+
+    df = df.select_dtypes(include=['number'])
+
     result_df = pd.DataFrame()
 
     names = list()
@@ -13,10 +40,13 @@ def check_numeric_data(df: pd.DataFrame, round: int = 1):
     q25s = list()
     q50s = list()
     q75s = list()
+    iqrs = list()
     mins = list()
     maxs = list()
     stds = list()
     sums = list()
+    lout = list()
+    uout = list()
 
     for column in df.columns:
         names.append(column)
@@ -26,10 +56,13 @@ def check_numeric_data(df: pd.DataFrame, round: int = 1):
         q25s.append(df[column].quantile(0.25))
         q50s.append(df[column].quantile(0.5))
         q75s.append(df[column].quantile(0.75))
+        iqrs.append(_count_iqr(df, column))
         mins.append(df[column].min())
         maxs.append(df[column].max())
         stds.append(df[column].std())
         sums.append(df[column].sum())
+        lout.append((df[column] < (df[column].quantile(0.25) - 1.5 * _count_iqr(df, column))).sum())
+        uout.append((df[column] > (df[column].quantile(0.75) + 1.5 * _count_iqr(df, column))).sum())
 
     result_df["NAME"] = names
     result_df["TYPE"] = types
@@ -38,11 +71,19 @@ def check_numeric_data(df: pd.DataFrame, round: int = 1):
     result_df['Q25'] = [f"{q25:.{round}f}" for q25 in q25s]
     result_df['Q50'] = [f"{q50:.{round}f}" for q50 in q50s]
     result_df['Q75'] = [f"{q75:.{round}f}" for q75 in q75s]
+    result_df["IQR"] = [f"{iqr:.{round}f}" for iqr in iqrs]
     result_df["MIN"] = [f"{min:.{round}f}" for min in mins]
     result_df["MAX"] = [f"{max:.{round}f}" for max in maxs]
     result_df["STD"] = [f"{std:.{round}f}" for std in stds]
     result_df["SUM"] = [f"{sum:.{round}f}" for sum in sums]
+    result_df["LOW OUT"] = lout
+    result_df["UPP OUT"] = uout
 
-    return result_df.to_string(index=False)
+    if use is True:
+        return result_df
+    if use is False:
+        return result_df.to_string(index=False)
 
 
+def _count_iqr(df, column):
+    return df[column].quantile(0.75) - df[column].quantile(0.25)
--- testing.py
+++ testing.py
@@ -3,10 +3,11 @@ from datamining.preprocessing import handle_NaN
 from datamining.check import check_numeric_data
 import pandas
 
-df = pd.DataFrame({'a': [None, None, 3, None, 3, 3, 3, 3, 3, 3c],
-                   'b': [2, 2, 3, 1, 2, 3, 1, 2, 1, 3]})
+df = pd.DataFrame({'a': [None, None, 3, None, 3 , 3, 3, 3, 3, 3],
+                   'b': [2.1, 2.1, 2.1, 2.1, 2.1, -88.2, 2.23, -8.99, 2.2, 2.23],
+                   'c': ["a", "b", "a", "b","a", "b","a", "b","a", "b",]})
 
 # print(handle_NaN(df, strategy=12))
 
 print(check_numeric_data(df, 2))
-print(df.describe())
+
