2bd972fb9133b47698738f5b1a34dac4a2ae81b1
filiphalys02 <filiphalys02@gmail.com>
2024-09-10 21:33:03 +0200

    Class BestSimpleRegression: finding best feature name and its r2 using train_test method

--- datamining/regression.py
+++ datamining/regression.py
@@ -1,15 +1,58 @@
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import r2_score, mean_squared_error
+
 from datamining._errors import _validate_argument_types1, _validate_inputs
 import pandas as pd
 
 
 class BestSimpleRegression:
-    def __init__(self, df: pd.DataFrame):
+    def __init__(self, df: pd.DataFrame, response: str):
         self._df = df
+        self._response = response
 
         self._validate_inputs()
+        self._find_best_feature()
 
     def _validate_inputs(self):
         dic = {
             "df": ["DataFrame", self._df]
         }
         _validate_inputs(dic)
+
+    def _find_best_feature(self):
+        """ Finds the best feature that explains the response variable using linear regression """
+
+        try:
+            self._df = self._df.select_dtypes(include=['number'])
+            self._df = self._df.select_dtypes(exclude=['timedelta', 'datetime'])
+            features = list(self._df.columns)
+            features.remove(self._response)
+        except ValueError:
+            raise ValueError(f"There is not '{self._response}' numeric column in your Data Frame")
+
+        y = self._df[self._response].values
+        self._best_feature_dic = {"r-squared": -1}
+
+        for feature in features:
+            x = self._df[feature].values
+            x = x.reshape(-1, 1)
+
+            self._train_test_for_find_best_feature(feature, x, y)
+            # print(f"Evaluated feature: {feature}, Current best feature dict: {self._best_feature_dic}")
+
+        self.best_feature = self._best_feature_dic["feature"]
+        # print(f"Best feature: {self.best_feature}, R-squared: {self._best_feature_dic['r-squared']}")
+
+    def _train_test_for_find_best_feature(self, feature, x, y):
+        x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2)
+        LinReg = LinearRegression()
+        LinReg.fit(x_train, y_train)
+        y_pred = LinReg.predict(x_test)
+
+        r2 = round(r2_score(y_test, y_pred), 4)
+
+        if r2 > self._best_feature_dic["r-squared"]:
+            self._best_feature_dic["r-squared"] = r2
+            self._best_feature_dic["feature"] = feature
+
--- testing.py
+++ testing.py
@@ -9,17 +9,16 @@ from datamining.regression import BestSimpleRegression
 
 pd.set_option('future.no_silent_downcasting', True)
 
-df = pd.DataFrame({'a': [None, None, None, None, 3 , 3, -3, 3, 3, 32],
-                   'b': [2.1, None, 2.1, 2.1, 2.1, -88.2, 2.23, -8.99, 2.2, 2.23],
+df = pd.DataFrame({'a': [3, 4, 5, 6, 3 , 3, -3, 3, 3, 32],
+                   'b': [2.1, 23, 2.1, 2.1, 2.1, -88.2, 2.23, -8.99, 2.2, 2.23],
+                   'j': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+                   'k': [2, 3, 3, 3, 5, 6, 7, 8, 9, 9],
                    'c': ["a", "b", "a", "b", "None", "b", "a", "b", "a", "b"],
-                   'd': pd.Categorical(['x', None, 'z', 'x', 'y', 'z', 'x', 'y', 'z', 's']),
-                   'e': pd.Series(['foo', 'bar', 'baz', None, 'bar', 'baz', 'foo', 'bar', 'baz', 'xxx'], dtype="string"),
-                   'f': [None, pd.Timestamp('2024-01-02 09:15:00'), pd.Timestamp('2024-01-02 09:15:00'), pd.Timestamp('2024-01-02 15:45:00'), pd.Timestamp('2024-01-03 07:00:00'), pd.Timestamp('2024-01-03 14:00:00'), pd.Timestamp('2024-01-04 10:00:00'), pd.Timestamp('2024-01-04 18:30:00'), pd.Timestamp('2024-01-05 08:45:00'), pd.Timestamp('2024-01-05 20:15:00')],
-                   'g': pd.date_range(start='2024-01-01', periods=10, freq='D'),
-                   'h': [True, np.nan, False, True, None, True, True, False, False, False],
-                   'i': [pd.Timedelta(hours=0), pd.Timedelta(hours=1), pd.Timedelta(hours=2), pd.Timedelta(hours=3), pd.Timedelta(hours=4), pd.Timedelta(hours=5), pd.Timedelta(hours=6), pd.Timedelta(hours=7), pd.Timedelta(hours=8), pd.Timedelta(hours=9)]
+                   'd': pd.Categorical(['x', 'z', 'z', 'x', 'y', 'z', 'x', 'y', 'z', 's']),
+                   'e': pd.Series(['foo', 'bar', 'baz', 'bar', 'bar', 'baz', 'foo', 'bar', 'baz', 'xxx'], dtype="string")
+                   #'f': [pd.Timestamp('2024-01-02 09:15:00'), pd.Timestamp('2024-01-02 09:15:00'), pd.Timestamp('2024-01-02 09:15:00'), pd.Timestamp('2024-01-02 15:45:00'), pd.Timestamp('2024-01-03 07:00:00'), pd.Timestamp('2024-01-03 14:00:00'), pd.Timestamp('2024-01-04 10:00:00'), pd.Timestamp('2024-01-04 18:30:00'), pd.Timestamp('2024-01-05 08:45:00'), pd.Timestamp('2024-01-05 20:15:00')],
+                   #'g': pd.date_range(start='2024-01-01', periods=10, freq='D'),
+                   #'i': [pd.Timedelta(hours=0), pd.Timedelta(hours=1), pd.Timedelta(hours=2), pd.Timedelta(hours=3), pd.Timedelta(hours=4), pd.Timedelta(hours=5), pd.Timedelta(hours=6), pd.Timedelta(hours=7), pd.Timedelta(hours=8), pd.Timedelta(hours=9)]
                    })
 
-# print(one_hot_encoding(df, ['c', 'd', 'h'], [17, 3], '-', False))
-
-BestSimpleRegression(df)
+print(BestSimpleRegression(df, 'j').best_feature)
