f9816eda09424e630683b8c3845c26f1a6386828
filiphalys02 <filiphalys02@gmail.com>
2024-11-20 20:06:45 +0100

    Changes in Engineering thesis

--- ET_codes.py
+++ ET_codes.py
@@ -1,8 +1,9 @@
 import pandas as pd
 
 from datamining._errors import _validate_method_argument_types, _validate_class_argument_types
-from datamining.check import check_numeric_data, check_category_data, check_time_series_data, check_time_interval_data, \
-    check_data
+from datamining.check import check_numeric_data, check_category_data, check_time_series_data, check_time_interval_data, check_data
+from datamining.transformations import standarization, normalization_min_max
+from datamining.regression import BestSimpleLinearRegression, BestMultipleLinearRegression
 
 ramka_testowa = pd.DataFrame({'a': [3, 4, 5, 6, 3, 3, -3, 3, 3, 32],
                               'b': [2.1, 23, 2.1, 2.1, 2.1, -88.2, 2.23, -8.99, 2.2, 2.23],
@@ -20,12 +21,28 @@ ramka_testowa = pd.DataFrame({'a': [3, 4, 5, 6, 3, 3, -3, 3, 3, 32],
                                     pd.Timestamp('2024-01-04 10:00:00'), pd.Timestamp('2024-01-04 18:30:00'),
                                     pd.Timestamp('2024-01-05 08:45:00'), pd.Timestamp('2024-01-05 20:15:00')],
                               'k': pd.date_range(start='2024-01-01', periods=10, freq='D'),
-                              'l': [pd.Timedelta(hours=0), pd.Timedelta(hours=1), pd.Timedelta(hours=2),
-                                    pd.Timedelta(hours=3),
-                                    pd.Timedelta(hours=4), pd.Timedelta(hours=5), pd.Timedelta(hours=6),
-                                    pd.Timedelta(hours=7),
-                                    pd.Timedelta(hours=8), pd.Timedelta(hours=9)]
+                              'l': [pd.Timedelta(hours=0, minutes=5, seconds=30),
+                                    pd.Timedelta(hours=1, minutes=10, seconds=15),
+                                    pd.Timedelta(hours=2, minutes=20, seconds=45),
+                                    pd.Timedelta(hours=3, minutes=15, seconds=0),
+                                    pd.Timedelta(hours=4, minutes=25, seconds=10),
+                                    pd.Timedelta(hours=5, minutes=30, seconds=30),
+                                    pd.Timedelta(hours=6, minutes=45, seconds=50),
+                                    pd.Timedelta(hours=7, minutes=50, seconds=20),
+                                    pd.Timedelta(hours=8, minutes=5, seconds=5),
+                                    pd.Timedelta(hours=9, minutes=15, seconds=35)],
+                              'm': [pd.Timedelta(days=5),
+                                    pd.Timedelta(days=10),
+                                    pd.Timedelta(days=15),
+                                    None,
+                                    pd.Timedelta(days=25),
+                                    pd.Timedelta(days=30),
+                                    pd.Timedelta(days=35),
+                                    pd.Timedelta(days=40),
+                                    pd.Timedelta(days=45),
+                                    pd.Timedelta(days=45)]
                               })
+
 # Opis podmodulu _errors
 '''
 @_validate_method_argument_types
@@ -52,5 +69,31 @@ instance = Test('a')
 # Opis podmodulu check
 '''
 print(check_numeric_data(df=ramka_testowa, round=2, use=False))
-'''
 print(check_category_data(df=ramka_testowa, use=False, cat_dist=True))
+print(check_time_series_data(df=ramka_testowa, use=False))
+print(check_time_interval_data(df=ramka_testowa, use=False))
+check_data(df=ramka_testowa)
+'''
+
+# Opis podmodulu normalizations
+'''
+standarization(df)
+normalization_min_max(df, ['a', 'b'])
+'''
+
+df = pd.DataFrame({
+    'a': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    'b': [-10, -2, 2, 10, 17, 23, 43, 111, 145, 243]
+})
+
+print(df)
+
+
+
+
+
+
+
+
+
+#BestMultipleLinearRegression(ramka_testowa, 'd').plot_model()
