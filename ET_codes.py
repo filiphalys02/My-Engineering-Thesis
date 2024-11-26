df3290aa4acdc815d0039094e1dcb2587e77866a
filiphalys02 <filiphalys02@gmail.com>
2024-11-26 20:50:02 +0100

    Changes in Engineering thesis

--- ET_codes.py
+++ ET_codes.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 from datamining._errors import _validate_method_argument_types, _validate_class_argument_types
 from datamining.check import check_numeric_data, check_category_data, check_time_series_data, check_time_interval_data, check_data
-from datamining.transformations import standarization, normalization_min_max, log_transformation, transformation_box_kox, root_transformation, binarization
+from datamining.transformations import standarization, normalization_min_max, log_transformation, transformation_box_kox, root_transformation, one_hot_encoding
 from datamining.regression import BestSimpleLinearRegression, BestMultipleLinearRegression
 
 ramka_testowa = pd.DataFrame({'a': [3, 4, 5, 6, 3, 3, -3, 3, 3, 32],
@@ -77,22 +77,21 @@ check_data(df=ramka_testowa)
 
 # Opis podmodulu normalizations
 '''
+df = pd.DataFrame({
+    'a': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    'b': [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+})
 standarization(df)
 normalization_min_max(df, ['a', 'b'])
 print(log_transformation(df, ['a', 'b'], 2.0,10.0,1.0))
 print(transformation_box_kox(df, columns=['a', 'b'], alpha=3))
 print(root_transformation(df, ['a', 'b'], root=2))
 print(binarization(df, ['a', 'b'], border=8, values=['<8', '>=8']))
-'''
-
 df = pd.DataFrame({
-    'a': [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
-    'b': [1, 4, 9, 16, 25, 36, 49, 64, 81, 100]
+    'Category': pd.Categorical(['x', 'z', 'z', 'x', 'y', 'z', 'x', 'y', 'z', 's'])
 })
-
-
-
-
+print(one_hot_encoding(df, columns=['Category'], values=[0, 1], prefix_sep='_', drop=True))
+'''
 
 
 
